505c4f29c8fdb5512d9ac397ce5753c08483561d
belikmaks <142260216+belikmaks@users.noreply.github.com>
2024-10-21 23:58:00 +0300

    Студент 1

---
+++ lab10.py
@@ -0,0 +1,22 @@
+#Функція відкриття файлов
+def openFile(file_name, mode):
+    try:
+        file = open(file_name, mode)
+    except:
+        print("Файл", file_name, "не відкрився")
+        return None;
+    else:
+        print("Файл", file_name, "відкрився")
+        return file;
+#Cтворення файлу
+file_name = "Python.txt"
+
+#Відкриття файлу
+file_name_w = openFile("Python.txt", "w")
+
+#Запис у файл
+if file_name_w is not None:
+     file_name_w.write("Бєлік Максим\n")
+     file_name_w.write("Чи є мова Python інтерпретованою?")
+     file_name_w.close()
+     print(f"Файл {file_name} закрито.")
